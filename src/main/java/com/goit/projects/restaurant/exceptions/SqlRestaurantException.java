baacd0ed3a013573f363629d6bc302588c065561
freedomserg <freedomserg97@gmail.com>
2016-06-04 00:47:27 +0300

    create custom exceptions: SqlRestaurantException

---
+++ src/main/java/com/goit/projects/restaurant/exceptions/SqlRestaurantException.java
@@ -0,0 +1,8 @@
+package com.goit.projects.restaurant.exceptions;
+
+public class SqlRestaurantException extends RuntimeException {
+
+    public SqlRestaurantException(String message) {
+        super(message);
+    }
+}
